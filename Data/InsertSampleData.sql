0fc2812c496efffe40853a59440674dc9241db84
cswisher42 <31708343+cswisher42@users.noreply.github.com>
2019-12-02 01:12:29 -0600

    Added more test data

--- Data/InsertSampleData.sql
+++ Data/InsertSampleData.sql
@@ -4,26 +4,28 @@ INSERT INTO Users.Roles ([Name], CanCreateTasks, CanAssignTasks, CanDeleteTasks,
 INSERT INTO Users.Roles ([Name], CanCreateTasks, CanAssignTasks, CanDeleteTasks, CanModifyTasks) values (N'Guest', 0, 0, 0, 0);
 
 --Users.Users
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('wcabrera0', 'msouthworth0@virginia.edu', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('abeddall1', 'pfeatonby1@timesonline.co.uk', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('esandal2', 'mhucknall2@live.com', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('kcarnell3', 'ssatchel3@php.net', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('twhorlton4', 'talven4@mozilla.org', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('bcrunkhurn5', 'dchisman5@reverbnation.com', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('brosson6', 'rflegg6@blogspot.com', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('sfleming7', 'dband7@drupal.org', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('afairest8', 'sbarclay8@yale.edu', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('semberson9', 'bbrockhouse9@stumbleupon.com', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('pweinera', 'rmuzzia@geocities.com', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('tpenniellb', 'iodlinb@xrea.com', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('rspowagec', 'mistonc@google.com.au', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('tgurnerd', 'lhickinbottomd@blogtalkradio.com', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('bderislye', 'rschottlie@whitehouse.gov', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('acumesf', 'llovef@unc.edu', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('asiggeryg', 'lkorneichukg@homestead.com', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('scoopeyh', 'cpatnellih@answers.com', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('emccarrelli', 'mcalwelli@yelp.com', PWDENCRYPT("test"));
-INSERT INTO Users.Users (Name, Email, PasswordHash) values ('fmcquodej', 'lcowthardj@comsenz.com', PWDENCRYPT("test"));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('wcabrera0', 'msouthworth0@virginia.edu', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('abeddall1', 'pfeatonby1@timesonline.co.uk', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('esandal2', 'mhucknall2@live.com', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('kcarnell3', 'ssatchel3@php.net', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('twhorlton4', 'talven4@mozilla.org', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('bcrunkhurn5', 'dchisman5@reverbnation.com', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('brosson6', 'rflegg6@blogspot.com', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('sfleming7', 'dband7@drupal.org', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('afairest8', 'sbarclay8@yale.edu', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('semberson9', 'bbrockhouse9@stumbleupon.com', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('pweinera', 'rmuzzia@geocities.com', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('tpenniellb', 'iodlinb@xrea.com', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('rspowagec', 'mistonc@google.com.au', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('tgurnerd', 'lhickinbottomd@blogtalkradio.com', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('bderislye', 'rschottlie@whitehouse.gov', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('acumesf', 'llovef@unc.edu', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('asiggeryg', 'lkorneichukg@homestead.com', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('scoopeyh', 'cpatnellih@answers.com', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('emccarrelli', 'mcalwelli@yelp.com', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('fmcquodej', 'lcowthardj@comsenz.com', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('demo', 'demo@demo.com', PWDENCRYPT('test'));
+INSERT INTO Users.Users (Name, Email, PasswordHash) values ('demo2', 'demo2@demo.com', PWDENCRYPT('test'));
 
 --Users.UserGroups
 INSERT INTO Users.UserGroups (GroupOwnerID, [Name], [Description]) values ((SELECT UserID FROM Users.Users WHERE [Name] = N'wcabrera0'), N'Desktop Support', N'Provide end user support');
@@ -31,6 +33,8 @@ INSERT INTO Users.UserGroups (GroupOwnerID, [Name], [Description]) values ((SELE
 INSERT INTO Users.UserGroups (GroupOwnerID, [Name], [Description]) values ((SELECT UserID FROM Users.Users WHERE [Name] = N'esandal2'), N'Networking and Security', N'Provide and maintain a network and its security');
 INSERT INTO Users.UserGroups (GroupOwnerID, [Name], [Description]) values ((SELECT UserID FROM Users.Users WHERE [Name] = N'kcarnell3'), N'Web', N'Maintain and update website');
 INSERT INTO Users.UserGroups (GroupOwnerID, [Name], [Description]) values ((SELECT UserID FROM Users.Users WHERE [Name] = N'twhorlton4'), N'Hardware', N'Build and repair computers');
+INSERT INTO Users.UserGroups (GroupOwnerID, [Name], [Description]) values ((SELECT UserID FROM Users.Users WHERE [Name] = N'demo2'), N'560Testing', N'Test group for the presentation');
+
 
 --Users.UserGroupUsers
 INSERT INTO Users.UserGroupUsers (UserGroupID, UserID, RoleID) values 
@@ -73,6 +77,10 @@ INSERT INTO Users.UserGroupUsers (UserGroupID, UserID, RoleID) values
 	((SELECT UserGroupID FROM Users.UserGroups WHERE [Name] = N'Hardware'), (SELECT UserID FROM Users.Users WHERE [Name] = N'emccarrelli'), (SELECT RoleID FROM Users.Roles WHERE [Name] = N'Member'));
 INSERT INTO Users.UserGroupUsers (UserGroupID, UserID, RoleID) values 
 	((SELECT UserGroupID FROM Users.UserGroups WHERE [Name] = N'Hardware'), (SELECT UserID FROM Users.Users WHERE [Name] = N'fmcquodej'), (SELECT RoleID FROM Users.Roles WHERE [Name] = N'Member'));
+INSERT INTO Users.UserGroupUsers (UserGroupID, UserID, RoleID) values 
+	((SELECT UserGroupID FROM Users.UserGroups WHERE [Name] = N'Hardware'), (SELECT UserID FROM Users.Users WHERE [Name] = N'demo'), (SELECT RoleID FROM Users.Roles WHERE [Name] = N'Member'));
+INSERT INTO Users.UserGroupUsers (UserGroupID, UserID, RoleID) values 
+	((SELECT UserGroupID FROM Users.UserGroups WHERE [Name] = N'560Testing'), (SELECT UserID FROM Users.Users WHERE [Name] = N'demo2'), (SELECT RoleID FROM Users.Roles WHERE [Name] = N'Owner'));
 
 --Tasks.TaskStates
 INSERT INTO Tasks.TaskStates ([Name], [Description], Color) values (N'Not Started', N'', N'D3D3D3');
